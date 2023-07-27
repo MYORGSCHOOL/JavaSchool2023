8a0cfdb70d6aeb168ba7a4b3ebc2bd9791d8b1d3
Nikita Koroliov <nik_iv02@mail.ru>
2023-07-27 18:20:20 +0700

    first commit
    Королёв/ Добавил Hello world
    
    Королёв/ Добавил Hello world на русском
    
    Королёв/ Добавил исправления

---
+++ Task1/src/main/java/Koroliov/Task1.java
@@ -0,0 +1,13 @@
+package Koroliov;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Add "Hello wolrd!"
+        System.out.println("Hello wolrd");
+        // Добавил "Hello wolrd!" на русском
+        System.out.printf("""
+                Измененеие
+                Выаода!
+                """);
+    }
+}
