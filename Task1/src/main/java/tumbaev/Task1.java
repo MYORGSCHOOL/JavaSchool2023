88aef621069d7aa5ca09ba5d4a0a6e2c0ac72da3
alekseytumbaev <leha.tumbaev@gmail.com>
2023-07-27 22:31:15 +0700

    Task1 Тумбаев Схлопнул три коммита

---
+++ Task1/src/main/java/tumbaev/Task1.java
@@ -0,0 +1,9 @@
+package tumbaev;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Привет мир!");
+        System.out.println("¡Hola Mundo!");
+    }
+}
