5956da6d71e2cfbc1291599fdaefe61320b7aa39
Piryazev Yury <ypiryazev@gmail.com>
2023-07-27 19:09:29 +0700

    Схлопнул коммит

---
+++ Task1/src/main/java/piryazev/Task1.java
@@ -0,0 +1,10 @@
+package piryazev;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String hello = "hello";
+        String world = "world!";
+        System.out.println(hello + " " + world);
+
+    }
+}
