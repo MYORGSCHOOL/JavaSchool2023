e312d4b1e94a6b3fbe38323cf134b61753dc9eee
yav <angelina12ya@gmail.com>
2023-07-28 12:06:03 +0700

    Task1 Янько Схлопнуты коммиты

---
+++ Task1/src/main/java/yanko/Task1.java
@@ -0,0 +1,9 @@
+package yanko;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello pes");
+        System.out.println("Hello kot");
+    }
+}
