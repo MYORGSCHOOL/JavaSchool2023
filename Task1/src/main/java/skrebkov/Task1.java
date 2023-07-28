8a194763ba22bbc62efa99aacbf852a4534091ea
Sergey Skrebkov <seruy103iruska@gmail.com>
2023-07-28 12:05:28 +0700

    Task1 Скребков схлопнул три комита

---
+++ Task1/src/main/java/skrebkov/Task1.java
@@ -0,0 +1,8 @@
+package skrebkov;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println("Привет мир!!!");
+    }
+}
