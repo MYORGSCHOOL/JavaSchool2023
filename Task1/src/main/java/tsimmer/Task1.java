3ce923f032067eacfc9495d479417350c261f533
Julia-A-7 <you@example.com>
2023-07-29 10:31:19 +0700

    Task1 Циммер схлопнула коммиты

---
+++ Task1/src/main/java/tsimmer/Task1.java
@@ -0,0 +1,9 @@
+package tsimmer;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("123");
+        System.out.println("java");
+    }
+}
