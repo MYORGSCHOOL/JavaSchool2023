80d711981b6287e9a4d4715687f10a40d6492f88
Бабаджанов Азамат <bimaister@mail.ru>
2023-07-31 15:33:51 +0300

    Task1 Бабаджанов allIn

---
+++ Task1/src/main/java/babadzhanov/Task1.java
@@ -0,0 +1,10 @@
+package babadzhanov;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+    }
+}
