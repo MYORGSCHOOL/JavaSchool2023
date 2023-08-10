107a5b3c18625da2c2350d5378e2c5a5a1c4b98f
yav <angelina12ya@gmail.com>
2023-08-10 10:22:57 +0700

    Task4 Янько Исправлены тесты

---
+++ Task4/src/main/java/yanko/Task4.java
@@ -0,0 +1,12 @@
+package yanko;
+
+public class Task4 {
+    public static void main(String[] args) {
+        CustomStringBuilder builder = new CustomStringBuilder();
+        builder.append("abcd");
+        builder.replace(0, 9, "0");
+        System.out.println(builder);
+        builder.rollback();
+        System.out.println(builder);
+    }
+}
