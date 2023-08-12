d67c6ba7aba2ef65da03b9883b7a684672145c93
EkaterinaKarmanchikova <ekarmanchikova@mail.ru>
2023-08-12 21:48:58 +0700

    Task3 Карманчикова реализация стека и очереди с тестами исправленный

---
+++ Task3/src/main/java/karmanchikova/Task3.java
@@ -0,0 +1,25 @@
+package karmanchikova;
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Queue queue = new Queue(3);
+        queue.enqueue(3);
+        queue.enqueue(2);
+        queue.enqueue(1);
+        System.out.println(queue);
+        System.out.println("Первый элемент в очереди: " + queue.top());
+        queue.dequeue();
+        System.out.println("Текущее состояние очереди: " + Arrays.toString(queue.getAll()));
+
+        Stack stack = new Stack(2);
+        stack.push(3);
+        stack.push(2);
+        stack.push(2);
+        System.out.println(stack);
+        stack.pop();
+        System.out.println("Верхний элемент в стеке: " + stack.top());
+        System.out.println("Текущее состояние стека: " + Arrays.toString(stack.getAll()));
+    }
+}
