cdf40cb548d1c24f57c3ac636c00793503d0b3fa
yav <angelina12ya@gmail.com>
2023-08-18 14:14:09 +0700

    Task5 Янько Добавлены интерфейс и реализация словаря с тестами
    Task5 Янько Внесены изменения в тесты и отредактированы комментарии
    
    Task5 Янько Исправлено название метода и определены константы
    
    Task5 Янько Исправлен метод swap

---
+++ Task5/src/main/java/yanko/Task5.java
@@ -0,0 +1,11 @@
+package yanko;
+
+public class Task5 {
+    public static void main(String[] args) {
+        DictionaryImpl<Integer> dictionary = new DictionaryImpl<>();
+        dictionary.insert(12);
+        System.out.println(dictionary.consist(12));
+        System.out.println(dictionary);
+
+    }
+}
