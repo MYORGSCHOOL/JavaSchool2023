87a7662e3b1f1627b83879793b901f2c5c78bad0
Nikita Koroliov <nik_iv02@mail.ru>
2023-08-20 13:28:51 +0700

    Task1 Королев Добавление документации, сдача

---
+++ Task2/src/main/java/koroliov/Task2.java
@@ -0,0 +1,107 @@
+package koroliov;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task2 {
+    public static Method met = new Method();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Vector vector1 = new Vector();
+        Vector vector2 = new Vector();
+        List<Vector> vectors = new ArrayList<>();
+        vectors.add(vector1);
+        vectors.add(vector2);
+        String result;
+        int choice = 0;
+        int n = 0;
+
+        while(true) {
+            System.out.println("Выберете операцию: ");
+            System.out.println("""
+                    1. Длинна вектора;
+                    2. Скалярное произведение веткторов;
+                    3. Векторное произведение;
+                    4. Вычислить угол между векторами;
+                    5. Вычислить сумму векторов;
+                    6. Вычислить разность векторов;
+                    7. Получить случайные векторы;
+                    8. Выход.
+                    """);
+            System.out.print(">> ");
+            choice = in.nextInt();
+
+            switch (choice) {
+                case 1:
+                // TODO: Длинна вектора
+                    vector1.input(vectors.get(0), in);
+                    
+                    result = String.format("%.3f", met.lengthVector(vector1));
+                    System.out.println("Длинна вектора = " + result);
+                    System.out.printf("\n\n");
+                    break;
+                
+                case 2:
+                // TODO: Скалярное произведение веткторов
+                    vector1.input(vectors.get(0), vectors.get(1), in);
+
+                    result = String.format("%.3f", met.scalarProduct(vector1, vector2));
+                    System.out.println("Скалярное произведение векторов = " + result);
+                    System.out.printf("\n\n");
+                    break;
+                
+                case 3:
+                // TODO: Векторное произведение
+                    vector1.input(vectors.get(0), vectors.get(1), in);
+
+                    System.out.println("Векторное произведение = " + met.vectorProduct(vector1, vector2));
+                    break;
+                
+                case 4:
+                // TODO: Угол между векторами
+                    vector1.input(vectors.get(0), vectors.get(1), in);
+
+                    result = String.format("%.3f", met.angleVectors(vector1, vector2));
+                    System.out.println("Угол между векторами = " + result);
+                    System.out.printf("\n\n");
+                    break;
+                
+                case 5:
+                // TODO: Сумма векторов
+                    vector1.input(vectors.get(0), vectors.get(1), in);
+
+                    System.out.println("Сумма векторов = " + met.amount(vector1, vector2));
+                    System.out.printf("\n\n");
+                    break;
+                
+                case 6:
+                // TODO: Разность
+                    vector1.input(vectors.get(0), vectors.get(1), in);
+
+                    System.out.println("Разность векторов = " + met.difference(vector1, vector2));
+                    System.out.printf("\n\n");
+                    break;
+                
+                case 7:
+                    System.out.print("Введите кол-во векторов: ");
+                    n = in.nextInt();
+                    List<Vector> generVercList = vector1.setVectors(n);
+
+                    System.out.println("Полученные вектора: ");
+                    for (Vector i : generVercList) System.out.println(i.setString(i));
+                    System.out.printf("\n\n");
+                    break;
+                case 8:
+                    System.out.println("Закрытие....");
+                    in.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Неверный ввод!!!");
+                    break;
+            }
+        }
+    }
+}
