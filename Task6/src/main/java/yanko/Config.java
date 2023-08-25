9458735187a6afd446c9b9838efdbdbc5d2ab616
yav <angelina12ya@gmail.com>
2023-08-25 13:06:35 +0700

    Task6 Янько Создан класс Config и метод loadConfigFromMap
    Task6 Янько Исправлен метод loadConfigFromMap, добавлены методы setConfigFieldValue, invokeConfigMethod и тесты для них
    
    Task6 Янько Исправлен метод loadConfigFromMap

---
+++ Task6/src/main/java/yanko/Config.java
@@ -0,0 +1,30 @@
+package yanko;
+
+/**
+ * класс, хранящий параметры конфигурации
+ */
+public class Config {
+    /**
+     * ключ api
+     */
+    private String apiKey;
+    /**
+     * максимальное количество подключений
+     */
+    private int maxConnections;
+    /**
+     * возможность использования режима отладки
+     */
+    private boolean debugMode;
+
+    /**
+     * печать текущей конфигурации
+     */
+    private void print() {
+        System.out.println("Config{" +
+                "apiKey='" + apiKey + '\'' +
+                ", maxConnections=" + maxConnections +
+                ", debugMode=" + debugMode +
+                '}');
+    }
+}
