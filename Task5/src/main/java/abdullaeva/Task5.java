76cc7c9f356b15637d5fe37db922581224e6a6d0
Вера Абдуллаева <sinang0601@gmail.com>
2023-09-08 01:31:27 +0700

    Task5 Абдуллаева Реализация generic интерфейса Dictionary с алгоритмической сложностью O(1) + тесты

--- Task5/src/main/java/abdullaeva/Task5.java
+++ Task5/src/main/java/abdullaeva/Task5.java
@@ -1,4 +1,16 @@
 package abdullaeva;
 
 public class Task5 {
+    public static void main(String[] args) {
+        DictionaryImpl<String> dictionary = new DictionaryImpl<>();
+        dictionary.insert("abc");
+        dictionary.insert("def");
+        System.out.println(dictionary.consist("abc"));
+        System.out.println(dictionary);
+        System.out.println(dictionary.getRandom());
+        dictionary.delete("abc");
+        System.out.println(dictionary);
+        dictionary.delete("def");
+        System.out.println(dictionary);
+    }
 }
